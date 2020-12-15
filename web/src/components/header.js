cb5429ff96030d60d31c4bddfe4c2f2b08be9d57
Benjamin Konopka <benjamin.konopka@icloud.com>
2020-12-15 14:33:09 +0100

    fix: 🐛 try something with graphql

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -21,6 +21,9 @@ const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
           <li>
             <Link to='/archive/'>Archive</Link>
           </li>
+          <li>
+            <Link to='/title/'>Title</Link>
+          </li>
         </ul>
       </nav>
     </div>
